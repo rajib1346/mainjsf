3b33ebf5f43685d56648cc99f9a114db82476bba
Rajib Kumar Halder <rajib.cse1346@gmail.com>
2017-05-15 02:55:15 +0600

    Update and rename seventhSemester.java to sevenSemesterNote.java

--- src/main/java/org/joinfaces/example/view/sevenSemesterNote.java
+++ src/main/java/org/joinfaces/example/view/sevenSemesterNote.java
@@ -5,6 +5,11 @@
  */
 package semester;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Connection;
@@ -14,10 +19,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+
 
 /**
  *
@@ -25,10 +37,12 @@ import javax.faces.context.FacesContext;
  */ 
 @ManagedBean
 @SessionScoped
-public class seventhSemester {
+public class sevenSemesterNote {
     
-        private String selectedname;
+    private String selectedname;
+private Scanner x;
 
+String line;
     public String getSelectedname() {
         return selectedname;
     }
@@ -45,24 +59,31 @@ public class seventhSemester {
         return list;
     }
 
-    public List<tabelElement> getLocation() throws ClassNotFoundException, SQLException {
+    public List<tabelElement> getLocation() throws  FileNotFoundException, IOException {
 
         List<tabelElement> list = new ArrayList<tabelElement>();
+       //  BufferedReader br=null;
 
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/seventh", "root", "");
+      //  br=new BufferedReader(new FileReader("C:\\Users\\Acer\\Documents\\NetBeansProjects\\NuCseHome\\text"));
+      
+//        Class.forName("com.mysql.jdbc.Driver");
+//        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast", "root", "");
+//
+//        Statement stmt = con.createStatement();
+//        ResultSet rs = stmt.executeQuery("select id,location,fileName,Author,size from " + selectedname + "");
+ URL oracle = new URL("http://shawonislam.com/rajib/fast.txt");
+    BufferedReader in = new BufferedReader(
+    new InputStreamReader(oracle.openStream()));
 
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("select id,location,fileName,Author,size from " + selectedname + "");
-
-        while (rs.next()) {
+    String inputLine;
+        while((inputLine = in.readLine()) != null) {
             tabelElement element = new tabelElement();
-
-            element.setId(rs.getString("id"));
-            element.setFileName(rs.getString("fileName"));
-            element.setLocation(rs.getString("location"));
-            element.setAuthor(rs.getString("Author"));
-            element.setSize(rs.getString("size"));
+              
+           // element.setId(rs.getString("id"));
+            element.setLocation(inputLine);
+//            element.setLocation(rs.getString("location"));
+//            element.setAuthor(rs.getString("Author"));
+//            element.setSize(rs.getString("size"));
 
             list.add(element);
         }
@@ -70,19 +91,19 @@ public class seventhSemester {
         return list;
     }
 
-    public void download(String lo, String name) {
+    public void download( String lo) {
 
         String l = lo;
-        String n = name;
+      //  String n = name;
         try {
             FacesContext context = FacesContext.getCurrentInstance();
             ExternalContext externalContext = context.getExternalContext();
 
             externalContext.responseReset();
             externalContext.setResponseContentType("application/pdf");
-            externalContext.setResponseHeader("Content-Disposition", "attachment;filename=\"" + n + "\"");
+            externalContext.setResponseHeader("Content-Disposition", "attachment;filename=\"" + l + "\"");
 
-            InputStream inputStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/seventh/" + l + "");
+            InputStream inputStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/images/" + l + "");
             OutputStream outputStream = externalContext.getResponseOutputStream();
 
             byte[] buffer = new byte[2048];
@@ -97,11 +118,5 @@ public class seventhSemester {
         } catch (Exception e) {
             e.printStackTrace(System.out);
         }
-    }
-
-    public String page(String tab) throws ClassNotFoundException, SQLException {
-
-      setSelectedname(tab);
-        return "7SS.xhtml";
     }
 }
