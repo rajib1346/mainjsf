e46ba7c61cfb63d091262a008c18b3a6f862ae70
Rajib Kumar Halder <rajib.cse1346@gmail.com>
2017-05-15 02:04:31 +0600

    Update and rename fifthSemesetr.java to fifthSemesterNote.java

--- src/main/java/org/joinfaces/example/view/fifthSemesterNote.java
+++ src/main/java/org/joinfaces/example/view/fifthSemesterNote.java
@@ -5,8 +5,14 @@
  */
 package semester;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -14,6 +20,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
@@ -26,10 +33,12 @@ import javax.faces.context.FacesContext;
 
 @ManagedBean
 @SessionScoped
-public class fifthSemesetr {
+public class fifthSemesterNote{
     
-        private String selectedname;
+            private String selectedname;
+private Scanner x;
 
+String line;
     public String getSelectedname() {
         return selectedname;
     }
@@ -46,24 +55,31 @@ public class fifthSemesetr {
         return list;
     }
 
-    public List<tabelElement> getLocation() throws ClassNotFoundException, SQLException {
+    public List<tabelElement> getLocation() throws  FileNotFoundException, IOException {
 
         List<tabelElement> list = new ArrayList<tabelElement>();
+       //  BufferedReader br=null;
 
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fifth", "root", "");
+      //  br=new BufferedReader(new FileReader("C:\\Users\\Acer\\Documents\\NetBeansProjects\\NuCseHome\\text"));
+      
+//        Class.forName("com.mysql.jdbc.Driver");
+//        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast", "root", "");
+//
+//        Statement stmt = con.createStatement();
+//        ResultSet rs = stmt.executeQuery("select id,location,fileName,Author,size from " + selectedname + "");
+ URL oracle = new URL("http://shawonislam.com/rajib/fast.txt");
+    BufferedReader in = new BufferedReader(
+    new InputStreamReader(oracle.openStream()));
 
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("select id,location,fileName,Author,size from " + selectedname + "");
-
-        while (rs.next()) {
+    String inputLine;
+        while((inputLine = in.readLine()) != null) {
             tabelElement element = new tabelElement();
-
-            element.setId(rs.getString("id"));
-            element.setFileName(rs.getString("fileName"));
-            element.setLocation(rs.getString("location"));
-            element.setAuthor(rs.getString("Author"));
-            element.setSize(rs.getString("size"));
+              
+           // element.setId(rs.getString("id"));
+            element.setLocation(inputLine);
+//            element.setLocation(rs.getString("location"));
+//            element.setAuthor(rs.getString("Author"));
+//            element.setSize(rs.getString("size"));
 
             list.add(element);
         }
@@ -71,19 +87,19 @@ public class fifthSemesetr {
         return list;
     }
 
-    public void download(String lo, String name) {
+    public void download( String lo) {
 
         String l = lo;
-        String n = name;
+      //  String n = name;
         try {
             FacesContext context = FacesContext.getCurrentInstance();
             ExternalContext externalContext = context.getExternalContext();
 
             externalContext.responseReset();
             externalContext.setResponseContentType("application/pdf");
-            externalContext.setResponseHeader("Content-Disposition", "attachment;filename=\"" + n + "\"");
+            externalContext.setResponseHeader("Content-Disposition", "attachment;filename=\"" + l + "\"");
 
-            InputStream inputStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/fifth/" + l + "");
+            InputStream inputStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/images/" + l + "");
             OutputStream outputStream = externalContext.getResponseOutputStream();
 
             byte[] buffer = new byte[2048];
@@ -100,9 +116,5 @@ public class fifthSemesetr {
         }
     }
 
-    public String page(String tab) throws ClassNotFoundException, SQLException {
-
-        setSelectedname(tab);
-        return "5SS.xhtml";
-    }
+  
 }
