11a50385f7d14ac6b9bcb32c5c69f2451e20e134
Rajib Kumar Halder <rajib.cse1346@gmail.com>
2017-05-17 02:44:50 +0600

    Update secondSemesterNote.java

--- src/main/java/org/joinfaces/example/view/secondSemesterNote.java
+++ src/main/java/org/joinfaces/example/view/secondSemesterNote.java
@@ -90,7 +90,65 @@ String line;
 
         return list;
     }
+ public List<tabelElement> getLocationQuestion() throws  FileNotFoundException, IOException {
 
+        List<tabelElement> list = new ArrayList<tabelElement>();
+       //  BufferedReader br=null;
+
+      //  br=new BufferedReader(new FileReader("C:\\Users\\Acer\\Documents\\NetBeansProjects\\NuCseHome\\text"));
+      
+//        Class.forName("com.mysql.jdbc.Driver");
+//        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast", "root", "");
+//
+//        Statement stmt = con.createStatement();
+//        ResultSet rs = stmt.executeQuery("select id,location,fileName,Author,size from " + selectedname + "");
+ URL oracle = new URL("http://shawonislam.com/rajib/fast.txt");
+    BufferedReader in = new BufferedReader(
+    new InputStreamReader(oracle.openStream()));
+
+    String inputLine;
+        while((inputLine = in.readLine()) != null) {
+            tabelElement element = new tabelElement();
+              
+           // element.setId(rs.getString("id"));
+            element.setQlocation(inputLine);
+//            element.setLocation(rs.getString("location"));
+//            element.setAuthor(rs.getString("Author"));
+//            element.setSize(rs.getString("size"));
+
+            list.add(element);
+        }
+
+        return list;
+    }
+   public void downloadQuestion( String lo) {
+
+        String l = lo;
+      //  String n = name;
+        try {
+            FacesContext context = FacesContext.getCurrentInstance();
+            ExternalContext externalContext = context.getExternalContext();
+
+            externalContext.responseReset();
+            externalContext.setResponseContentType("application/pdf");
+            externalContext.setResponseHeader("Content-Disposition", "attachment;filename=\"" + l + "\"");
+
+            InputStream inputStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/fast/" + l + "");
+            OutputStream outputStream = externalContext.getResponseOutputStream();
+
+            byte[] buffer = new byte[2048];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+            inputStream.close();
+            context.responseComplete();
+
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
     public void download( String lo) {
 
         String l = lo;
